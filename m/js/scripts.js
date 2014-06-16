ad3b483e507f596dc0063c894414f657b345218d
Andrew McGill <arm5077@gmail.com>
2014-06-16 13:46:14 -0400

    Adds back button to mobile site, other fixes

--- m/js/scripts.js
+++ m/js/scripts.js
@@ -1,54 +1,99 @@
 var buttonTimer, swipeTimer, swipeX0;
 
 // *****************************************
-// Intro behavior 
+// Loading behavior 
 
-d3.select(".intro .button")
-	.on("touchstart", function(){
-		d3.event.preventDefault();
-		d3.select(".intro .button .fa-globe").style("opacity", 1);
-		buttonTimer = setTimeout(function(){
-			loadBrowse(function(){
-				d3.select(".intro").style("top", "-100%");
-				d3.select(".browse").style("top", "0");
-				d3.select(".browse .header").style("position", "fixed");
-				d3.select(".intro").style("display", "none");
-			});
-		}, 2000);
-	})
-	.on("touchend", function(){
-		clearTimeout(buttonTimer);
-		d3.select(".intro .button .fa-globe").style("opacity", .25);
-	})
-	//REMOVE THIS FOR LIVE
-	.on("click", function(){
+hashChange();
+
+window.onhashchange = function(){ hashChange(); }
+
+// End loading behavior
+// *****************************************
+
+// *****************************************
+// Hash event behavior
+
+function hashChange(){
+	console.log("changed");
+	if( window.location.hash == "") {
+		loadIntro();
+	}
+	else if( window.location.hash == "#countries" ) {
 		loadBrowse(function(){
-			d3.select(".intro").style("top", "-100%");
-			d3.select(".browse").style("top", "0");
-			d3.select(".browse .header").style("position", "fixed");
-			d3.select(".intro").style("display", "none");
-		});
-	});
-
-d3.select(".intro")
-	.on("touchstart", function(){
-		d3.event.preventDefault();
-		swipeX0 = d3.event.changedTouches[0].clientX;
-	})
-	.on("touchend", function() {
-			
-			
-			if( d3.event.changedTouches[0].clientX - swipeX0 < -50)
-			{
+			d3.select(".browse")
+				.style("top", "0")
+				.style("right", "0");
+			d3.select(".browse .header").style("display", "block")
 				
-				d3.select(".intro")
-					.style({"right":"100%", "opacity":0});
+			d3.select(".intro").style("top", "-100%");
+			
+			d3.select(".story")
+				.style({"left":"100%"})
+				.style("display", "none");	
+		});
+		
+	}
+	else {
+		d3.select(".intro").style("top", "-100%");
+		loadStory(window.location.hash.slice(1), function(){
+			d3.select(".story")
+				.style("display", "block")
+				.style("left", "0");
+			window.scrollTo(0,0);
+			d3.select(".browse").style("right", "100%");
+			d3.select(".header").style("display", "none");
+		});
+	}
+
+
+}
+
+// End hash event
+// *****************************************
+
+
+// *****************************************
+// Intro behavior 
+function loadIntro(callback) {
+	d3.select(".intro .button")
+		.on("touchstart", function(){
+			d3.event.preventDefault();
+			d3.select(".intro .button .fa-globe").style("opacity", 1);
+			buttonTimer = setTimeout(function(){
+				window.location.hash = "countries";
+			}, 2000);
+		})
+		.on("touchend", function(){
+			clearTimeout(buttonTimer);
+			d3.select(".intro .button .fa-globe").style("opacity", .25);
+		})
+		//REMOVE THIS FOR LIVE
+		.on("click", function(){
+			loadBrowse(function(){
+				window.location.hash = "countries";
+			});
+		});
 	
-				d3.select(".tellUs")
-					.style("left", "0");
-			}
-	});
-	
+	d3.select(".intro")
+		.on("touchstart", function(){
+			d3.event.preventDefault();
+			swipeX0 = d3.event.changedTouches[0].clientX;
+		})
+		.on("touchend", function() {
+				
+				
+				if( d3.event.changedTouches[0].clientX - swipeX0 < -50)
+				{
+					
+					d3.select(".intro")
+						.style({"right":"100%", "opacity":0});
+		
+					d3.select(".tellUs")
+						.style("left", "0");
+				}
+		});
+		if(typeof callback === 'function') callback();
+}	
 // End Intro behavior 
 // *****************************************
 
@@ -57,28 +102,23 @@ d3.select(".intro")
 // *****************************************
 // Start browse behavior 
 function loadBrowse(callback) {
-	history.pushState();
- 	window.location.hash = 'countries';
-	d3.json("../php/getNations.php?operation=getActiveCountries", function(error, results){
-		d3.select(".browse").append("ul")
-		.selectAll("li")
-			.data(results).enter()
-		.append("li")
-			.attr("class", function(d){ return d.Count > 0 ? "" : "inactive" })
-			.html(function(d){ return d.Country });
 	
-	
-		d3.selectAll(".browse li")
-		.on("click", function(d){
-			loadStory(d.Country, function() {
-				d3.select(".story").style("display", "block");
-				window.scrollTo(0,0);
-				d3.select(".browse").style("right", "100%");
-				d3.select(".header").style("left", "-110%");
-				d3.select(".story").style("left", "0");
+	if(d3.selectAll(".browse li").node() == null) {
+		d3.json("../php/getNations.php?operation=getActiveCountries", function(error, results){
+			d3.select(".browse").append("ul")
+			.selectAll("li")
+				.data(results).enter()
+			.append("li")
+				.attr("class", function(d){ return d.Count > 0 ? "" : "inactive" })
+				.html(function(d){ return d.Country });
+		
+		
+			d3.selectAll(".browse li")
+			.on("click", function(d){
+				window.location.hash = d.Country;
 			});
 		});
-	});
+	}
 	if(typeof callback === 'function') callback();
 }
 
@@ -92,13 +132,12 @@ function loadBrowse(callback) {
 
 function loadStory(country, callback) {
 	
+	d3.select(".story video").remove();
+	
 	$.getJSON("../php/getNations.php?operation=getSingleCountry&country=" + country, function(data){ 
 		
 		formattedName = data[0].Country.toLowerCase().replace(/ /g, "_");
-		
-		window.location.hash = formattedName;
-		history.pushState();
-		
+	
 		d3.select(".story h1")
 			.text(data[0].Country);
 		
@@ -117,7 +156,8 @@ function loadStory(country, callback) {
 		
 		
 		console.log(formattedName);
-		d3.select(".story video").attr("src", "../countries/" + formattedName + "/vid/" + formattedName + "_portrait.mp4");
+		d3.select(".story").insert("video", "h2")
+			.attr("src", "../countries/" + formattedName + "/vid/" + formattedName + "_portrait.mp4");
 		
 		// Activate swipe to go back to browse page
 		d3.select(".story")
@@ -131,21 +171,15 @@ function loadStory(country, callback) {
 					if( d3.event.changedTouches[0].clientX - swipeX0 > 100)
 					{
 						
-						d3.select(".story")
-							.style({"left":"100%", "opacity":1});
-			
-						d3.select(".browse")
-							.style("right", "0");
-							
-						d3.select(".header")
-							.style("left", "0");
-						
-						d3.select(".story").style("display", "none");						
+						window.location.hash = "countries";			
 							
 					}
 			});
+	
+		if(typeof callback === 'function') callback();
+		
 	});
-	if(typeof callback === 'function') callback();
+	
 }
 // End story behavior 
 // *****************************************
